4951785e8fddc1b7a1ba7622ee58f79c9b00885b
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-23 15:13:15 -0600

    feat: add filter to show first 20 messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,16 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
     return unless message.save
 
-    redirect_to root_path
+    ActionCable.server.broadcast 'chatroom_channel', mod_message: message_render(message)
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: message })
   end
 end
